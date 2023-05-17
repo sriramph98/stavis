2e61af7962957d31de88f481597ba256efe45f61
Sriram P H <sriramh9812@gmail.com>
2023-05-17 16:28:06 +0530

    added floating form

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //onScroll Animation
 
 window.addEventListener('scroll', function() {
-    var image = document.getElementById('rotateAnimation');
+    var image = document.getElementById('rotateAnimation1');
+    var image2 = document.getElementById('rotateAnimation2');
     var scrollAmount = window.pageYOffset;
     
     // Adjust the rotation amount as desired
     var rotationAmount = scrollAmount / 10;
     
     image.style.transform = 'rotate(' + rotationAmount + 'deg)';
+    image2.style.transform = 'rotate(' + rotationAmount + 'deg)';
   });
+
+  //Pop-Up Modal
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
